d84633e0377c7563f0f8d6e6c70d2319fee88bef
happyincent <happyincent@gmail.com>
2023-02-20 23:46:49 +0800

    Bump to version 1.1

--- quick-pick-no.user.js
+++ quick-pick-no.user.js
@@ -3,7 +3,7 @@
 // @description 車牌號碼吉凶判斷
 // @namespace   https://github.com/happyincent/QuickPickNo
 // @match       https://www.mvdis.gov.tw/m3-emv-plate/webpickno/queryPickNo
-// @version     1.0
+// @version     1.1
 // @author      happyincent
 // @homepageURL https://github.com/happyincent/QuickPickNo
 // @require     https://cdn.jsdelivr.net/npm/sweetalert2@11
